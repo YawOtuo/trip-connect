f6fb1295c9a53b48f402ec87a1584659515c659e
03kwAd <okyere97747@gmail.com>
2024-07-08 15:07:09 +0000

    notifications

---
+++ src/app/dashboard/notifications/page-preview.tsx
@@ -0,0 +1,8 @@
+// Transportrix/pages/notifications/page-preview.tsx
+import Page from '../../../src/app/dashboard/notifications/page';
+
+const PagePreview = () => {
+  return <Page />;
+};
+
+export default PagePreview;
