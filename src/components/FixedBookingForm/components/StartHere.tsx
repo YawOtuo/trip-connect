736ea811bdfe2dd4c33b924b99a1a324b222f413
JesseMurah <jkhnmurah@gmail.com>
2024-07-10 00:31:35 +0000

    Fixed booking: For Jesse's part completed

--- src/components/FixedBookingForm/components/StartHere.tsx
+++ src/components/FixedBookingForm/components/StartHere.tsx
@@ -1,8 +1,46 @@
 import FramerWrapper from "@/components/FramerWrapper";
 import { fadeIn } from "@/lib/animations";
+import Image from "next/image";
 
 function StartHere() {
-  return <FramerWrapper {...fadeIn}>Start here ( Jesse)</FramerWrapper>;
+  return (
+    <FramerWrapper {...fadeIn} className="h-full">
+      <div className="h-full flex flex-col justify-center items-start border p-4 rounded-lg shadow-lg h-200">
+        <p className="mb-4  text-2xl lg:text-5xl text-primary font-bold">
+          Select a bus
+        </p>
+        <div className="flex space-x-4 w-full">
+          <div className="relative w-1/3 h-40">
+            <Image
+              src="/buses1.jpg"
+              alt="Bus 1"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="relative w-1/3 h-40">
+            <Image
+              src="/buses1.jpg"
+              alt="Bus 2"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="relative w-1/3 h-40">
+            <Image
+              src="/buses1.jpg"
+              alt="Bus 3"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </FramerWrapper>
+  );
 }
 
 export default StartHere;
