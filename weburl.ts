e9af1efc52f1d08c7bee02ce26fa0d234438e3fb
YawOtuo <yotuo2003@gmail.com>
2024-07-15 20:11:06 +0000

    weburl

--- weburl.ts
+++ weburl.ts
@@ -1,3 +1,3 @@
-    // export const url: string = "https://trip-connect-e7497c974b59.herokuapp.com/"
+    export const url: string = "https://trip-connect-e7497c974b59.herokuapp.com/"
 
-    export const url: string = "http://127.0.0.1:8000/"
+    // export const url: string = "http://127.0.0.1:8000/"
