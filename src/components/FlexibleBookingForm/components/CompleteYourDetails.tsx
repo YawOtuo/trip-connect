314d2c89ac0d87d03193a4648a046821a2a0cd80
Sharonmee <enamyawlui@gmail.com>
2024-07-16 22:47:07 +0000

    first draft

--- src/components/FlexibleBookingForm/components/CompleteYourDetails.tsx
+++ src/components/FlexibleBookingForm/components/CompleteYourDetails.tsx
@@ -11,6 +11,61 @@ function CompleteYourDetails() {
                 type="text" name="first-name"
                 />
             </div>
+
+
+            <div>
+                <TextInput label="Last Name" placeholder="Enter Your Last Name"
+                type="text" name="Last-name"
+                />
+            </div>
+
+
+            <div>
+                <TextInput label="Date of Birth" placeholder="Enter Your Date of Birth"
+                type="drop box" name="Date-of-birth"
+                />
+            </div> 
+
+
+            <div>
+                <TextInput label="Email" placeholder="example@gmail.comm"
+                type="text" name="Email"
+                />
+            </div>
+
+
+            <div>
+                <TextInput label="Phone Number" placeholder="000 000 0000"
+                type="text" name="Phone-Number"
+                />
+            </div>
+
+
+            <div>
+                <TextInput label="Where are you travelling from?" placeholder="eg Accra"
+                type="text" name="Location"
+                />
+            </div>
+
+
+            <div>
+                <TextInput label="Driver's License Number" placeholder="0000 0000"
+                type="text" name="License-Number"
+                />
+            </div>
+
+
+            <div>
+                <TextInput label="Driver's License Date of Expiry" 
+                type="Drop-box" name=" Date-of-Expiry"
+                />
+            </div>
+
+            <div>
+                <TextInput label="Ghana Card Number" 
+                type="Text" name="Ghana-Card-Number"
+                />
+            </div>
         </div>
     );
 }
