8f37ea512982e534bb2bbe922e9a0d31f97c1a65
YawOtuo <yotuo2003@gmail.com>
2024-07-17 13:54:55 +0000

    "Updated SelectVehicles component to access vehicle model name, added model and type objects to Vehicle interface, and modified primary color in tailwind.config.ts"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#6C194F",
           foreground: "hsl(var(--primary-foreground))",
           100: "#250118",
         },
