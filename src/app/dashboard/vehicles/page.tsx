8a4a90bb1c3b2b8dcfe240a147b3db72974e5565
YawOtuo <yotuo2003@gmail.com>
2024-07-19 12:40:51 +0000

    "Updated CSS class for container div in Vehicles component"

--- src/app/dashboard/vehicles/page.tsx
+++ src/app/dashboard/vehicles/page.tsx
@@ -6,7 +6,7 @@ function Vehicles() {
   const { flexibleBookings, isflexibleBookingsLoading } =
     useGetFlexibleBookings();
   return (
-    <div className="flex flex-col gap-5 px-5">
+    <div className="flex flex-col gap-5 md:px-5">
       <p className="text-2xl text-primary font-bold">My Vehicles</p>
       <div className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 gap-5">
         {flexibleBookings?.map((r) => (
