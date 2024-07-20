1f33af163594cbedbd84df15cc9829667b0eb1ce
jabbey4 <113597057+jabbey4@users.noreply.github.com>
2024-07-20 10:12:04 +0000

    Update Congratulations.tsx

--- src/components/FixedBookingForm/components/Congratulations.tsx
+++ src/components/FixedBookingForm/components/Congratulations.tsx
@@ -9,7 +9,19 @@ function Congrutalations() {
 
   return (
     <FramerWrapper {...fadeIn}>
-      Congrats (Kwartekwei)
+      <div className="flex items-center justify-center bg-white p-4">
+        {/* Changed justify-between to justify-center */}
+        <div className="max-w-lg">
+          <h1 className="text-4xl font-bold text-primary mb-4">Congratulations</h1>
+          <p className="mb-8 text-black-600">
+            Lorem ipsum dolor sit amet consectetur. Aliquam tincidunt fringilla pellentesque risus
+            erat quisque posuere ullamcorper cursus. Mi interdum est sed pharetra..
+          </p>
+        </div>
+        <div className="w-100">
+          <Image src="/fist-bump.png" alt="Fist bump" width={300} height={300} />
+        </div>
+      </div>
       {isSubmitting && <Submitting />}
       <FixedBookingControls showBackButton={false} close={true}/>
     </FramerWrapper>
