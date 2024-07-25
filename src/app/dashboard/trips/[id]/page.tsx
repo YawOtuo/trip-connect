e615d4d4e2aac7feab52013b346919b7068a9a56
YawOtuo <yotuo2003@gmail.com>
2024-07-25 12:51:40 +0000

    "Refactor trip page layout and add TripOrderDetails component"

--- src/app/dashboard/trips/[id]/page.tsx
+++ src/app/dashboard/trips/[id]/page.tsx
@@ -9,6 +9,7 @@ import useFixedBooking from "@/lib/hooks/useFixedBookings";
 import moment from "moment";
 import { FaBus, FaLongArrowAltRight } from "react-icons/fa";
 import { TbCoins } from "react-icons/tb";
+import TripOrderDetails from "./components/TripOrderDetails";
 
 type Props = {
   params: { id: number };
@@ -17,60 +18,31 @@ function Page({ params }: Props) {
   const { OneFixedBookings } = useFixedBooking(params?.id);
   const { handlePayment } = useTrips();
   return (
-    <>
+    <div className="h-full">
       {OneFixedBookings ? (
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center gap-5 text-2xl text-primary font-bold">
-            <p> {OneFixedBookings.bus_and_schedule.schedule.travelling_from}</p>
-            <FaLongArrowAltRight />
-            <p> {OneFixedBookings.bus_and_schedule.schedule.travelling_to}</p>
-          </div>
+        <div className="flex flex-col gap-5 w-full items-center justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 h-full w-full justify-center items-center">
+            {OneFixedBookings && (
+              <div className="relative aspect-[3/2] w-full overflow-hidden rounded-xl border-2">
+                <Image
+                  fill
+                  objectFit="cover"
+                  alt={OneFixedBookings.bus_and_schedule.transportbus.bus_type}
+                  src={`/Bismarkbus.png`}
+                />
+              </div>
+            )}
 
-          <div className="flex flex-col gap-1">
-            <p className="text-sm">
-              Date Booked:{" "}
-              {moment(OneFixedBookings.booking_date).format(
-                "dddd Do MMMM YYYYY"
-              )}
-            </p>
-            <p>
-              Departure Time: &nbsp;
-              {moment(
-                OneFixedBookings.bus_and_schedule.schedule.departure_time
-              ).format("dddd Do MMMM YYYY")}
-            </p>
-            <p>
-              Estimated Arrival Time:{" "}
-              {moment(
-                OneFixedBookings.bus_and_schedule.schedule
-                  .estimated_arrival_time
-              ).format("dddd Do MMMM YYYY")}
-            </p>
-
-            <div className="flex items-center gap-1">
-              <TbCoins />
-              <p>{OneFixedBookings.cost}</p>
+            <div className="flex flex-col gap-1 justify-end">
+              {/* <VehicleDetails vehicle={OneFixedBookings.vehicle} /> */}
+              <TripOrderDetails data={OneFixedBookings} />
             </div>
-            <div className="flex items-center gap-2">
-              <FaBus />
-              <p> {OneFixedBookings.bus_and_schedule.transportbus.bus_type}</p>
-            </div>
-          </div>
-
-          <div>
-            <div className="flex flex-col h-fit">
-              <CheckoutButton
-                onClick={() =>
-                  handlePayment(OneFixedBookings.cost, OneFixedBookings.id)
-                }
-              />
-            </div>{" "}
           </div>
         </div>
       ) : (
         <p>Loading Data...</p>
       )}{" "}
-    </>
+    </div>
   );
 }
 
